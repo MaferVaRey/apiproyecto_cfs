5c57ae154a6cea9ed607720d0207ae5a39840251
FerVanegas <mafevarey@hotmail.com>
2025-05-11 10:58:49 -0500

    3 endpoints preguntas
    Se crearon los endopoints para la búsqueda por id, la modificación y la eliminación de preguntas.

--- src/routes/preguntas.js
+++ src/routes/preguntas.js
@@ -17,6 +17,30 @@ router.get("/preguntas", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+//Consulta de preguntas por su id
+router.get("/preguntas/:id", (req, res) => {
+    const { id } = req.params;
+    preguntasSchema.findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
+//Modificar una pregunta por su id
+router.put("/preguntas/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    preguntasSchema.updateOne({ _id: id }, {
+            $set: { pregunta, solucionA, solucionB, solucionC, solucionD, respuestaCorrecta }})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar una pregunta por su id
+router.delete("/preguntas/:id", (req, res) => {
+    const { id } = req.params;
+    preguntasSchema.findByIdAndDelete(id)
+        .then((data) => {res.json(data);})
+        .catch((error) => {res.json({ message: error });});
+});
 
 module.exports = router;
