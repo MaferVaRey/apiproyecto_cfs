17c955d45ea1886f1172453a1dce17eaa6cc41b0
CarlR0P <davidrp123@hotmail.com>
2025-05-12 01:49:55 -0500

    Modificación de archivo índex
    Se modifica el archivo index.js para que acepte las rutas del usuario tambien. Además de implementa pequeño manejo de rutas no encontradas.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const preguntasRoutes = require("./routes/preguntas.js");
-//const authRoutes = require("./routes/authentication")
+const usuarioRoute = require("./routes/usuarioRoute")
 const mongoose = require("mongoose");
+
 require('dotenv').config();
+
 app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
 app.use(express.json());
 app.use("/api", preguntasRoutes);
+app.use("/api", usuarioRoute);
 
 mongoose
     .connect(process.env.MONGODB_URI)
@@ -17,4 +20,11 @@ mongoose
     .catch((error) => console.log(error));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-});
+});s
+
+// Manejo de rutas no encontradas
+app.all('*', (req, res) =>
+  res.status(404).json({ status: 'fail', message: `Ruta ${req.originalUrl} no existe.` })
+);
+
+module.exports = app;
