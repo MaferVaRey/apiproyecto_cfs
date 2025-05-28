69c14ff1d1cb3c35d566b086dd85b1513aa672fb
CarlR0P <davidrp123@hotmail.com>
2025-05-28 01:49:12 -0500

    Ruta de Partida
    Se soluciona problema con la definición de una constante que impedía el funcionamiento correcto de la ruleta en el Frontend.

--- src/routes/partida.js
+++ src/routes/partida.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const  Partida  = require("../models/partida");
-const { Pregunta } = require("../models/preguntas");
+const  Pregunta  = require("../models/preguntas");
 const  Categoria  = require("../models/categoria");
 
 
